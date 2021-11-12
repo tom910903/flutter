252d78e748af3b5e38e3d9b6e3f83429bd3bdee7
daeyoung.lee <tom910903@gmail.com>
2021-11-13 00:36:40 +0900

    add List item in Drawer

--- study/lib/main.dart
+++ study/lib/main.dart
@@ -50,6 +50,16 @@ class MyPage extends StatelessWidget {
               backgroundImage: AssetImage('assets/Apeach.png'),
               backgroundColor: Colors.white,
             ),
+            otherAccountsPictures: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/Ryan.gif'),
+                backgroundColor: Colors.white,
+              ),
+              // CircleAvatar(
+              //   backgroundImage: AssetImage('assets/Apeach.png'),
+              //   backgroundColor: Colors.white,
+              // ),
+            ],
             accountName: Text('Name'),
             accountEmail: Text('email@eamil.com'),
             onDetailsPressed: () {
@@ -60,7 +70,40 @@ class MyPage extends StatelessWidget {
                 borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(40),
                     bottomRight: Radius.circular(40))),
-          )
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.home,
+              color: Colors.grey[850],
+            ),
+            title: Text('Home'),
+            onTap: () {
+              print('Home is clicked');
+            },
+            trailing: Icon(Icons.add),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              color: Colors.grey[850],
+            ),
+            title: Text('Setting'),
+            onTap: () {
+              print('Setting is clicked');
+            },
+            trailing: Icon(Icons.add),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.question_answer,
+              color: Colors.grey[850],
+            ),
+            title: Text('Q&A'),
+            onTap: () {
+              print('Q&A is clicked');
+            },
+            trailing: Icon(Icons.add),
+          ),
         ]),
       ),
     );
