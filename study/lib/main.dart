aaa48c058f5f1add48fc409a2cd1174e70384018
daeyoung.lee <tom910903@gmail.com>
2021-11-12 21:55:53 +0900

    add drawer

--- study/lib/main.dart
+++ study/lib/main.dart
@@ -18,30 +18,51 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('Appbar Icon Menu'),
-      centerTitle: true,
-      elevation: 0.0,
-      leading: IconButton(
-        icon: Icon(Icons.menu),
-        onPressed: () {
-          print("menu button clicked");
-        },
+      appBar: AppBar(
+        title: Text('Appbar Icon Menu'),
+        centerTitle: true,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            print("menu button clicked");
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print("shopping_cart button clicked");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("search button clicked");
+            },
+          ),
+        ],
       ),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {
-            print("shopping_cart button clicked");
-          },
-        ),
-        IconButton(
-          icon: Icon(Icons.search),
-          onPressed: () {
-            print("search button clicked");
-          },
-        ),
-      ],
-    ));
+      drawer: Drawer(
+        child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+          UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: AssetImage('assets/Apeach.png'),
+              backgroundColor: Colors.white,
+            ),
+            accountName: Text('Name'),
+            accountEmail: Text('email@eamil.com'),
+            onDetailsPressed: () {
+              print('arrow is clicked');
+            },
+            decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40))),
+          )
+        ]),
+      ),
+    );
   }
 }
