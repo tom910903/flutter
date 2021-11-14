9f187779577b1a4ee2f7d51ce32f2c1c7392a715
daeyoung.lee <tom910903@gmail.com>
2021-11-15 07:00:29 +0900

    Snack Bar Study

--- study/lib/main.dart
+++ study/lib/main.dart
@@ -1,4 +1,4 @@
-//14
+//18
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AppBar',
+      title: 'Snack Bar',
       theme: ThemeData(primarySwatch: Colors.red),
       home: MyPage(),
     );
@@ -18,94 +18,26 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Appbar Icon Menu'),
-        centerTitle: true,
-        elevation: 0.0,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print("menu button clicked");
-          },
+        appBar: AppBar(
+          title: Text('AppBar'),
+          centerTitle: true,
         ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              print("shopping_cart button clicked");
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print("search button clicked");
-            },
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(padding: EdgeInsets.zero, children: <Widget>[
-          UserAccountsDrawerHeader(
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: AssetImage('assets/Apeach.png'),
-              backgroundColor: Colors.white,
-            ),
-            otherAccountsPictures: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/Ryan.gif'),
-                backgroundColor: Colors.white,
+        body: Builder(
+          builder: (BuildContext ctx) {
+            return Center(
+              child: FlatButton(
+                child: Text(
+                  "Show me",
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.red,
+                onPressed: () {
+                  Scaffold.of(ctx)
+                      .showSnackBar(SnackBar(content: Text("hello")));
+                },
               ),
-              // CircleAvatar(
-              //   backgroundImage: AssetImage('assets/Apeach.png'),
-              //   backgroundColor: Colors.white,
-              // ),
-            ],
-            accountName: Text('Name'),
-            accountEmail: Text('email@eamil.com'),
-            onDetailsPressed: () {
-              print('arrow is clicked');
-            },
-            decoration: BoxDecoration(
-                color: Colors.red[200],
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40))),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.home,
-              color: Colors.grey[850],
-            ),
-            title: Text('Home'),
-            onTap: () {
-              print('Home is clicked');
-            },
-            trailing: Icon(Icons.add),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.settings,
-              color: Colors.grey[850],
-            ),
-            title: Text('Setting'),
-            onTap: () {
-              print('Setting is clicked');
-            },
-            trailing: Icon(Icons.add),
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.question_answer,
-              color: Colors.grey[850],
-            ),
-            title: Text('Q&A'),
-            onTap: () {
-              print('Q&A is clicked');
-            },
-            trailing: Icon(Icons.add),
-          ),
-        ]),
-      ),
-    );
+            );
+          },
+        ));
   }
 }
