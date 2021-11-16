2f8a3929259ae452f75091ec72f3a617ee10b045
daeyoung.lee <tom910903@gmail.com>
2021-11-16 22:26:19 +0900

    modify snackbar

--- study/lib/main.dart
+++ study/lib/main.dart
@@ -22,22 +22,28 @@ class MyPage extends StatelessWidget {
           title: Text('AppBar'),
           centerTitle: true,
         ),
-        body: Builder(
-          builder: (BuildContext ctx) {
-            return Center(
-              child: FlatButton(
-                child: Text(
-                  "Show me",
-                  style: TextStyle(color: Colors.white),
-                ),
-                color: Colors.red,
-                onPressed: () {
-                  Scaffold.of(ctx)
-                      .showSnackBar(SnackBar(content: Text("hello")));
-                },
-              ),
-            );
-          },
-        ));
+        body: MySnackbar());
+  }
+}
+
+class MySnackbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text('show me'),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text(
+              'hello',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.teal,
+            duration: Duration(milliseconds: 1000),
+          ));
+        },
+      ),
+    );
   }
 }
