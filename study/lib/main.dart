4c39ce48e2dddd88ac7342a0fa6dec9f94913dde
daeyoung.lee <tom910903@gmail.com>
2021-11-22 21:42:49 +0900

    login page

--- study/lib/main.dart
+++ study/lib/main.dart
@@ -1,9 +1,4 @@
-//22
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:study/ScreenA.dart';
-import 'package:study/ScreenB.dart';
-import 'package:study/ScreenC.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,98 +6,100 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Snack Bar',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: MyPage(),
+      debugShowCheckedModeBanner: false,
+      title: 'Dice game',
+      home: LogIn(),
     );
   }
 }
 
-class MyPage extends StatelessWidget {
+
+class LogIn extends StatefulWidget {
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Scaffold Messenger'),
+        title: Text('Log in'),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: (){}
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: (){}
+          )
+        ],
       ),
-      body: HomeBody(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.thumb_up),
-        onPressed: () {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            content: Text("Like a new Snack bar"),
-            duration: Duration(seconds: 5),
-            action: SnackBarAction(
-              label: 'undo',
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => ThirdPage()));
-              },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(padding: EdgeInsets.only(top:50)),
+            Center(
+              child: Image(
+                image: AssetImage('image/chef.gif'),
+                width: 170,
+                height: 190,
+              ),
             ),
-          ));
-        },
-      ),
-    );
-  }
-}
-
-class HomeBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        child: Text('Go to second page'),
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SecondPage()));
-        },
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Page'),
-      ),
-      body: Text(
-        '"좋아요가 추가되었습니다."',
-        style: TextStyle(fontSize: 20, color: Colors.redAccent),
-      ),
-    );
-  }
-}
-
-class ThirdPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ScaffoldMessenger(
-        child: Scaffold(
-            appBar: AppBar(
-              title: Text('Third Page'),
-            ),
-            body: Builder(builder: (context) {
-              return Center(
+            Form(
+              child: Theme(
+                data: ThemeData(
+                    primaryColor: Colors.teal,
+                    inputDecorationTheme: InputDecorationTheme(
+                        labelStyle: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 15,
+                        )
+                    )
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(40),
                   child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                    Text(
-                      '"좋아요를 취소하시겠습니까?"',
-                      style: TextStyle(fontSize: 20, color: Colors.redAccent),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text('좋아요가 취소되었습니다.'),
-                          duration: Duration(seconds: 3),
-                        ));
-                      },
-                      child: Text("취소하기"),
-                    ),
-                  ]));
-            })));
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter "dice"',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Enter Password',
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(height: 40,),
+                      ButtonTheme(
+                        minWidth: 100,
+                          height: 50,
+                          child: RaisedButton(
+                            color: Colors.orangeAccent,
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                                size: 35,
+                              ),
+                              onPressed: (){}
+                          )
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
