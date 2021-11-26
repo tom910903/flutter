515dc527ef5d3f2d400494a6b37a5a7ddd20a9d1
daeyoung.lee <tom910903@gmail.com>
2021-11-26 23:19:49 +0900

    firebase init

--- study/lib/main.dart
+++ study/lib/main.dart
@@ -1,159 +1,132 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dice.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Dice game',
+      title: 'Login app',
+      theme: ThemeData(primarySwatch: Colors.grey),
       home: LogIn(),
     );
   }
 }
 
-class LogIn extends StatefulWidget {
-  @override
-  State<LogIn> createState() => _LogInState();
-}
-
-class _LogInState extends State<LogIn> {
-  TextEditingController controller = TextEditingController();
-  TextEditingController controller2 = TextEditingController();
+class LogIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Log in'),
-        backgroundColor: Colors.redAccent,
+        backgroundColor: Colors.blue,
+        title: Text(
+          'Sign In',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.search), onPressed: () {})
-        ],
+        elevation: 0.2,
       ),
-      body: Builder(
-        builder: (context) {
-          return GestureDetector(
-            onTap: (){
-              FocusScope.of(context).unfocus();
-            },
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(padding: EdgeInsets.only(top: 50)),
-                  Center(
-                    child: Image(
-                      image: AssetImage('image/chef.gif'),
-                      width: 170,
-                      height: 190,
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ButtonTheme(
+              height: 50.0,
+              child: RaisedButton(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Image.asset('images/glogo.png'),
+                    Text(
+                      'Login with Google',
+                      style: TextStyle(color: Colors.black87, fontSize: 15.0),
                     ),
-                  ),
-                  Form(
-                    child: Theme(
-                      data: ThemeData(
-                          primaryColor: Colors.teal,
-                          inputDecorationTheme: InputDecorationTheme(
-                              labelStyle: TextStyle(
-                            color: Colors.teal,
-                            fontSize: 15,
-                          ))),
-                      child: Container(
-                        padding: EdgeInsets.all(40),
-                        child: Column(
-                          children: [
-                            TextField(
-                              controller: controller,
-                              decoration: InputDecoration(
-                                labelText: 'Enter "dice"',
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                            ),
-                            TextField(
-                              controller: controller2,
-                              decoration: InputDecoration(
-                                labelText: 'Enter Password',
-                              ),
-                              keyboardType: TextInputType.text,
-                              obscureText: true,
-                            ),
-                            SizedBox(
-                              height: 40,
-                            ),
-                            ButtonTheme(
-                                minWidth: 100,
-                                height: 50,
-                                child: RaisedButton(
-                                    color: Colors.orangeAccent,
-                                    child: Icon(
-                                      Icons.arrow_forward,
-                                      color: Colors.white,
-                                      size: 35,
-                                    ),
-                                    onPressed: () {
-                                      if (controller.text == 'dice' &&
-                                          controller2.text == '1234') {
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (BuildContext context) =>
-                                                    Dice()));
-                                      } else if (controller.text == 'dice' &&
-                                          controller2.text != '1234') {
-                                        showSnackBar2(context);
-                                      } else if (controller.text != 'dice' &&
-                                          controller2.text == '1234') {
-                                        showSnackBar3(context);
-                                      } else {
-                                        showSnackBar(context);
-                                      }
-                                    }))
-                          ],
-                        ),
-                      ),
+                    Opacity(
+                      opacity: 0.0,
+                      child: Image.asset('images/glogo.png'),
                     ),
-                  )
-                ],
+                  ],
+                ),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(4.0),
+                ),
               ),
             ),
-          );
-        },
+            SizedBox(
+              height: 10.0,
+            ),
+            ButtonTheme(
+              height: 50.0,
+              child: RaisedButton(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Image.asset('images/flogo.png'),
+                    Text(
+                      'Login with Facebook',
+                      style: TextStyle(color: Colors.white, fontSize: 15.0),
+                    ),
+                    Opacity(
+                      opacity: 0.0,
+                      child: Image.asset('images/glogo.png'),
+                    ),
+                  ],
+                ),
+                color: Color(0xFF334D92),
+                onPressed: () {},
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(4.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            ButtonTheme(
+              height: 50.0,
+              child: RaisedButton(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      'Login with Email',
+                      style: TextStyle(color: Colors.white, fontSize: 15.0),
+                    ),
+                    Opacity(
+                      opacity: 0.0,
+                      child: Icon(
+                        Icons.mail,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                color: Colors.green,
+                onPressed: () {},
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(4.0),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
-
-void showSnackBar(BuildContext context) {
-  Scaffold.of(context).showSnackBar(SnackBar(
-    content: Text(
-      '로그인 정보를 다시 확인하세요.',
-      textAlign: TextAlign.center,
-    ),
-    duration: Duration(seconds: 2),
-    backgroundColor: Colors.blue,
-  ));
-}
-
-void showSnackBar2(BuildContext context) {
-  Scaffold.of(context).showSnackBar(SnackBar(
-    content: Text(
-      '비밀번호가 일치하지 않습니다.',
-      textAlign: TextAlign.center,
-    ),
-    duration: Duration(seconds: 2),
-    backgroundColor: Colors.blue,
-  ));
-}
-
-void showSnackBar3(BuildContext context) {
-  Scaffold.of(context).showSnackBar(SnackBar(
-    content: Text(
-      'dice의 철자를 확인하세요.',
-      textAlign: TextAlign.center,
-    ),
-    duration: Duration(seconds: 2),
-    backgroundColor: Colors.blue,
-  ));
 }
