83338d268e15df01b66ffcbf6a022a27def31602
daeyoung.lee <tom910903@gmail.com>
2021-12-02 20:27:56 +0900

    http & json

--- study/lib/screens/loading.dart
+++ study/lib/screens/loading.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,14 +11,45 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+    fetchData();
+  }
+
   void getLocation() async{
 
+    Map<Permission, PermissionStatus> permissions = await [Permission.locationWhenInUse].request();
 
-    Map<Permission, PermissionStatus> permissions = await [Permission.location].request();
+    try {
+      if (permissions[Permission.locationWhenInUse] ==
+          PermissionStatus.granted) {
+        Position position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high);
+        print(position);
+      }
+    }catch(e){
+      print('Error!!!');
+    }
+  }
 
+  void fetchData() async {
+    Response response = await get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
 
-    // Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    // print(position);
+    if(response.statusCode == 200){
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      print(myJson);
+
+      var wind = jsonDecode(jsonData)['wind']['speed'];
+      print(wind);
+
+      var id = jsonDecode(jsonData)['id'];
+      print(id);
+
+    }
 
   }
 
@@ -25,9 +58,11 @@ class _LoadingState extends State<Loading> {
     return Scaffold(
       body: Center(
         child: RaisedButton(
-          onPressed: (){
-            getLocation();
-          },
+          onPressed: null
+          //     (){
+          //   getLocation();
+          // }
+          ,
           child: Text(
             'Get my location',
             style: TextStyle(
