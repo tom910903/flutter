8a2ae87ee0bc985fe0f28fa01ca0fd2d156e49e3
daeyoung.lee <tom910903@gmail.com>
2021-12-12 12:56:28 +0900

    chat app UI

--- study/lib/main.dart
+++ study/lib/main.dart
@@ -1,21 +1,21 @@
-//15
 import 'package:flutter/material.dart';
-import 'package:study/screens/loading.dart';
+import 'screens//main_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather app',
+      title: 'Chatting app',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+          primarySwatch: Colors.blue
       ),
-      home: Loading(),
+      home: LoginSignupScreen(),
     );
   }
 }
