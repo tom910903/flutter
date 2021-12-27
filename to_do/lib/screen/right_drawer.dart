03bc0251ad76cbab9b9c5bf1fcd3dc8aca3f1f5b
daeyoung.lee <tom910903@gmail.com>
2021-12-28 00:01:07 +0900

    create right drawer

---
+++ to_do/lib/screen/right_drawer.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class RightDrawer extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      child: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('images/Apeach.png'),
+                backgroundColor: Colors.white,
+              ),
+              accountName: Text('Name'),
+              accountEmail: Text('email@eamil.com'),
+              // onDetailsPressed: () {
+              //   print('arrow is clicked');
+              // },
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.2,
+              height: MediaQuery.of(context).size.width * 0.2,
+              margin: EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 4),
+                        color: Colors.grey,
+                        spreadRadius: 3,
+                        blurRadius: 10
+                    )
+                  ]
+              ),
+              child: IconButton(
+                  onPressed: (){
+                    print("share");
+                  },
+                  icon: Icon(Icons.share,size: 50,)
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.2,
+              height: MediaQuery.of(context).size.width * 0.2,
+              margin: EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 4),
+                        color: Colors.grey,
+                        spreadRadius: 3,
+                        blurRadius: 10
+                    )
+                  ]
+              ),
+              child: IconButton(
+                  onPressed: (){
+                    print("shopping_cart");
+                  },
+                  icon: Icon(Icons.shopping_cart,size: 50,)
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.2,
+              height: MediaQuery.of(context).size.width * 0.2,
+              margin: EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 4),
+                        color: Colors.grey,
+                        spreadRadius: 3,
+                        blurRadius: 10
+                    )
+                  ]
+              ),
+              child: IconButton(
+                  onPressed: (){
+                    print("shop");
+                  },
+                  icon: Icon(Icons.addchart_rounded,size: 50,)
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.2,
+              height: MediaQuery.of(context).size.width * 0.2,
+              margin: EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 4),
+                        color: Colors.grey,
+                        spreadRadius: 3,
+                        blurRadius: 10
+                    )
+                  ]
+              ),
+              child: IconButton(
+                  onPressed: (){
+                    print("settings");
+                  },
+                  icon: Icon(Icons.settings,size: 50,)
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
