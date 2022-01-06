e0a6a26a7089b5d1ec6ab175c1d37c013445f041
daeyoung.lee <tom910903@gmail.com>
2022-01-06 22:52:34 +0900

    refactoring add to do text field

---
+++ to_do/lib/component/add_to_do_text_field.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+import 'package:to_do/model/to_do_model.dart';
+import 'package:to_do/database/to_do_bloc.dart';
+
+class AddToDoTextField extends StatefulWidget {
+
+  bool isAddToDo = false;
+  _AddToDoTextFieldState _addToDoTextFieldState = _AddToDoTextFieldState(false);
+
+  @override
+  _AddToDoTextFieldState createState() => _addToDoTextFieldState;
+
+  bool getIsAddToDo() {
+    return _addToDoTextFieldState.isAddToDo;
+  }
+
+  void setIsAddToDo(bool isAddToDo) {
+    _addToDoTextFieldState.isAddToDo = isAddToDo;
+  }
+}
+
+class _AddToDoTextFieldState extends State<AddToDoTextField> {
+  final ToDoBloc _todoBloc = ToDoBloc();
+  bool _isAddToDo = false;
+
+  _AddToDoTextFieldState(this._isAddToDo);
+
+  bool get isAddToDo{
+    return _isAddToDo;
+  }
+
+  void set isAddToDo(bool isAddToDo){
+    setState(() {
+      _isAddToDo = isAddToDo;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: isAddToDo,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height * 0.15,
+        child: Column(
+          children: [
+            TextFormField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isAddToDo = false;
+
+                        ToDoModel newTodo = ToDoModel(todo: '메모 내용이양', type: '했어!', complete: 1);
+                        _todoBloc.addToDos(newTodo);
+                      });
+                    },
+                    icon:Icon(Icons.arrow_circle_up,
+                      color: Colors.grey,
+                    )
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(35.0),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide:
+                  BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(35.0),
+                  ),
+                ),
+                fillColor: Colors.white,
+                filled: true,
+                hintText: 'Add To Do',
+                hintStyle: TextStyle(
+                    fontSize: 20, color: Colors.grey),
+                contentPadding: EdgeInsets.all(10),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.calendar_today)
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.folder_open)
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.auto_awesome_motion_rounded )
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.star_outline_rounded)
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.edit_rounded)
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- to_do/lib/screen/main_screen.dart
+++ to_do/lib/screen/main_screen.dart
@@ -3,9 +3,8 @@ import 'package:percent_indicator/percent_indicator.dart';
 
 import 'package:to_do/component/title_bar.dart';
 import 'package:to_do/component/right_drawer.dart';
-import 'package:to_do/model/to_do_model.dart';
-import 'package:to_do/database/to_do_bloc.dart';
 import 'package:to_do/screen/to_do_screen.dart';
+import 'package:to_do/component/add_to_do_text_field.dart';
 
 class MainScreen extends StatefulWidget {
   @override
@@ -13,14 +12,13 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  bool _isAddToDo = false;
-  final ToDoBloc todoBloc = ToDoBloc();
+  late AddToDoTextField _addToDoTextField;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _addToDoTextField = AddToDoTextField();
   }
 
   @override
@@ -83,101 +81,16 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
       floatingActionButton: Visibility(
-        visible: !_isAddToDo,
+        visible: !_addToDoTextField.getIsAddToDo(),
         child: FloatingActionButton(
           onPressed: () {
-            setState(() {
-              _isAddToDo = true;
-            });
+              _addToDoTextField.setIsAddToDo(true);
           },
           backgroundColor: Colors.white,
           child: const Icon(Icons.add, size: 50, color: Colors.blue,),
         ),
       ),
-      bottomSheet: Visibility(
-        visible: _isAddToDo,
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.15,
-          child: Column(
-            children: [
-              TextFormField(
-                autofocus: true,
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _isAddToDo = false;
-
-                          ToDoModel newTodo = ToDoModel(todo: '메모 내용이양', type: '했어!', complete: 1);
-                          todoBloc.addToDos(newTodo);
-                        });
-                      },
-                      icon:Icon(Icons.arrow_circle_up,
-                        color: Colors.grey,
-                      )
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(35.0),
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                    BorderSide(color: Colors.blue),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(35.0),
-                    ),
-                  ),
-                  fillColor: Colors.white,
-                  filled: true,
-                  hintText: 'Add To Do',
-                  hintStyle: TextStyle(
-                      fontSize: 20, color: Colors.grey),
-                  contentPadding: EdgeInsets.all(10),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: IconButton(
-                        onPressed: (){},
-                        icon: Icon(Icons.calendar_today)
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                        onPressed: (){},
-                        icon: Icon(Icons.folder_open)
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                        onPressed: (){},
-                        icon: Icon(Icons.auto_awesome_motion_rounded )
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                        onPressed: (){},
-                        icon: Icon(Icons.star_outline_rounded)
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                        onPressed: (){},
-                        icon: Icon(Icons.edit_rounded)
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+      bottomSheet: _addToDoTextField,
     );
   }
 }
