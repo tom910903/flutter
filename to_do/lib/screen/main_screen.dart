3dc62e6f9a6c089fb4348bcee572b48e9a4b3499
daeyoung.lee <tom910903@gmail.com>
2022-01-09 11:50:01 +0900

    fixed bug add to do text field

--- to_do/lib/screen/main_screen.dart
+++ to_do/lib/screen/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 import 'package:to_do/component/title_bar.dart';
 import 'package:to_do/component/right_drawer.dart';
@@ -29,68 +30,73 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: TitleBar(),
-      endDrawer: RightDrawer(),
-      endDrawerEnableOpenDragGesture: true,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 4,
-              child: PageView(
-                children: [
-                  GridView.count(
-                    crossAxisCount: 2,
-                    childAspectRatio: 1.8,
-                    children: List.generate(4, (index) {
-                      return GestureDetector(
-                        onTap: (){
-                          // _scaffoldKey.currentState!.openEndDrawer();
-                        },
-                        child: Card(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(Icons.add_a_photo_sharp),
-                                Text("data")
-                              ],
-                            )
-                        ),
-                      );
-                    }),
-                  )
-                  // Text("page2",),
-                  // Text("page3",),
-                ],
+    return
+      KeyboardDismissOnTap(
+      child: Scaffold(
+        appBar: TitleBar(),
+        endDrawer: RightDrawer(),
+        endDrawerEnableOpenDragGesture: true,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 4,
+                child: PageView(
+                  children: [
+                    GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.8,
+                      children: List.generate(4, (index) {
+                        return GestureDetector(
+                          onTap: (){
+                            // _scaffoldKey.currentState!.openEndDrawer();
+                          },
+                          child: Card(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.add_a_photo_sharp),
+                                  Text("data")
+                                ],
+                              )
+                          ),
+                        );
+                      }),
+                    )
+                    // Text("page2",),
+                    // Text("page3",),
+                  ],
+                ),
               ),
-            ),
-            Expanded(
-              flex: 6,
-              child: ToDoScreen(),
-            ),
-            LinearPercentIndicator(
-              lineHeight: 14.0,
-              percent: 0.5,
-              backgroundColor: Colors.grey,
-              progressColor: Colors.blue,
-            ),
-          ],
+              Expanded(
+                flex: 6,
+                child: ToDoScreen(),
+              ),
+              LinearPercentIndicator(
+                lineHeight: 14.0,
+                percent: 0.5,
+                backgroundColor: Colors.grey,
+                progressColor: Colors.blue,
+              ),
+            ],
+          ),
         ),
+        floatingActionButton: KeyboardVisibilityBuilder(builder: (context, visible){
+          return Visibility(
+            visible: !visible,
+            child: FloatingActionButton(
+              onPressed: () {
+                _addToDoTextField.setIsAddToDo(true);
+              },
+              backgroundColor: Colors.white,
+              child: const Icon(Icons.add, size: 50, color: Colors.blue,),
+            ),
+          );
+        }),
+        bottomSheet: _addToDoTextField,
       ),
-      floatingActionButton: Visibility(
-        visible: !_addToDoTextField.getIsAddToDo(),
-        child: FloatingActionButton(
-          onPressed: () {
-              _addToDoTextField.setIsAddToDo(true);
-          },
-          backgroundColor: Colors.white,
-          child: const Icon(Icons.add, size: 50, color: Colors.blue,),
-        ),
-      ),
-      bottomSheet: _addToDoTextField,
     );
   }
 }
