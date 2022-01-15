2df0daa1fc9f8db221903eeaa6aa7f5f2cf54b68
daeyoung.lee <tom910903@gmail.com>
2022-01-15 20:05:28 +0900

    modify login screen to ios

--- to_do/lib/main.dart
+++ to_do/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screen/login_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginScreen(),
+    return ScreenUtilInit(
+      designSize: Size(360,640),
+      builder: () => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginScreen(),
+      )
     );
+
   }
 }
--- to_do/lib/screen/login_screen.dart
+++ to_do/lib/screen/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do/screen/main_screen.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -7,161 +8,171 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(50,80,50,30),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(
-                width: 200,
-                height: 200,
-                child: Image.asset('images/img_logo.png', fit:BoxFit.contain),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.email,
-                    color: Colors.grey,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(35.0),
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                    BorderSide(color: Colors.blue),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(35.0),
-                    ),
-                  ),
-                  hintText: 'user@email.com',
-                  hintStyle: TextStyle(
-                      fontSize: 14, color: Colors.grey),
-                  contentPadding: EdgeInsets.all(10),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(30.w, 80.h, 30.w, 50.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              // mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 180.h,
+                  child: Image.asset(
+                      'images/img_logo.png', fit: BoxFit.contain),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                decoration:  InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    color: Colors.grey,
+                SizedBox(
+                  height: 10.h,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.email,
+                      color: Colors.grey,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(35.0),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.blue),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(35.0),
+                      ),
+                    ),
+                    hintText: 'user@email.com',
+                    hintStyle: TextStyle(
+                        fontSize: 14, color: Colors.grey),
+                    contentPadding: EdgeInsets.all(10.w),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(35.0),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    // errorText: 'ID or Password are not matched',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.grey,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(35.0),
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                      BorderSide(color: Colors.blue),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(35.0),
+                      ),
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(35.0),
+                      ),
+                    ),
+                    hintText: 'password',
+                    hintStyle: TextStyle(
+                        fontSize: 14, color: Colors.grey),
+                    contentPadding: EdgeInsets.all(10.w),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.h,
+                ),
+                SizedBox(
+                  height: 40.h,
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) {
+                            return MainScreen();
+                          })
+                      );
+                    },
+                    child: Text("Log in",
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                    ),
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(35.0),
+                        )
                     ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                    BorderSide(color: Colors.blue),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(35.0),
-                    ),
-                  ),
-                  hintText: 'user@email.com',
-                  hintStyle: TextStyle(
-                      fontSize: 14, color: Colors.grey),
-                  contentPadding: EdgeInsets.all(10),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text("ID or Password are not matched",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.red
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                height: 50,
-                child: TextButton(
-                  onPressed: (){
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context){
-                          return MainScreen();
-                        })
-                    );
-                  },
-                  child: Text("Log in",
+                TextButton(
+                  onPressed: () {},
+                  child: Text("Finding ID or PW",
                     style: TextStyle(
-                        color: Colors.white
-                    ),
-                  ),
-                  style: TextButton.styleFrom(
-                      backgroundColor: Colors.black,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(35.0),
-                      )
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: (){},
-                child: Text("Finding ID or PW",
-                  style: TextStyle(
-                      color: Colors.black,
-                      decoration: TextDecoration.underline
-                  ),
-                ),
-                style: TextButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(35.0),
-                    )
-                ),
-              ),
-              Text("Or sign in with : ",
-                textAlign: TextAlign.center,
-              ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children : [
-                    IconButton(onPressed: (){}, icon: Icon(Icons.add_shopping_cart)),
-                    VerticalDivider(thickness: 1, color: Colors.black,),
-                    IconButton(onPressed: (){}, icon: Icon(Icons.add_shopping_cart)),
-                  ]
-              ),
-              SizedBox(
-                height: 50,
-                child: TextButton(
-                  onPressed: (){},
-                  child: Text("Sign in",
-                    style: TextStyle(
-                      color: Colors.black,
+                        color: Colors.black,
+                        decoration: TextDecoration.underline
                     ),
                   ),
                   style: TextButton.styleFrom(
                       backgroundColor: Colors.white,
                       shape: RoundedRectangleBorder(
-                        side: BorderSide(
-                            color: Colors.black,
-                        ),
                         borderRadius: BorderRadius.circular(35.0),
                       )
                   ),
                 ),
-              ),
-            ],
+                Text("Or sign in with : ",
+                  textAlign: TextAlign.center,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(onPressed: () {},
+                          icon: Icon(Icons.add_shopping_cart,
+                            size: 40.w,)),
+                      VerticalDivider(
+                        thickness: 1, color: Colors.black,),
+                      IconButton(onPressed: () {},
+                          icon: Icon(Icons.add_shopping_cart,
+                            size: 40.w,)),
+                    ]
+                ),
+                SizedBox(
+                  height: 10.w,
+                ),
+                SizedBox(
+                  height: 40.h,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text("Sign in",
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: Colors.black,
+                          ),
+                          borderRadius: BorderRadius.circular(35.0),
+                        )
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
